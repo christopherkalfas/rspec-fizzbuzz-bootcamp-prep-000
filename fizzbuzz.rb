cc6ff93f80630925ffc7b0b5444e9572a71402f5
Christopher Kalfas <chriskalfas@yahoo.com>
2019-02-06 17:08:04 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,12 +5,12 @@ def fizzbuzz(num)
       "Fizz"
    
   elsif num % 5 == 0
-    puts "Buzz"
+    "Buzz"
   
   elsif num % 3 && 5 == 0
-    puts "FizzBuzz"
+     "FizzBuzz"
   
   else 
-    puts "nil"
+     "nil"
   end
 end
