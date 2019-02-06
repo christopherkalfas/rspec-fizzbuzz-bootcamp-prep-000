617e0c8f612c30a785faacb7895abdc63d554687
Christopher Kalfas <chriskalfas@yahoo.com>
2019-02-06 16:51:41 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,17 +2,12 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
   if num % 3 == 0
-    puts "Fizz"
-  
-    elsif num % 5 == 0
+     puts "Fizz"
+  elsif num % 5 == 0
     puts "Buzz"
-  
-    elsif num % 3 || 5 == 0
+  elsif num % 3 || 5 == 0
     puts "FizzBuzz"
-  
   else 
-  
     puts "nil"
-
 end
   
