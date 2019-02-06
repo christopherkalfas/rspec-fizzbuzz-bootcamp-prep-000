87f90e7a92e79b60f3f6fb448fbb46eab91fd878
Christopher Kalfas <chriskalfas@yahoo.com>
2019-02-06 17:18:48 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
-  if 
+  if num % 3 == 0 and num % 5 == 0
+     puts "FizzBuzz"
    
   elsif num % 5 == 0
     "Buzz"
   
-  elsif 
-  num % 3 == 0
+  elsif num % 3 == 0
       "Fizz"
   else 
     puts "nil"
